ad2359e68195d58e968907efcfaca0b406591aa3
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2023-08-15 12:25:08 +0200

    Add syncthing configuration

---
+++ .chezmoiscripts/run_onchange_after_configure-syncthing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Disable global discovery
+# https://docs.syncthing.net/users/config#config-option-options.globalannounceenabled
+syncthing cli config options global-ann-enabled set false
+# Disable NAT traversal
+# https://docs.syncthing.net/users/config#config-option-options.natenabled
+syncthing cli config options natenabled set false
+# Disable usage of relays
+# https://docs.syncthing.net/users/config#config-option-options.relaysenabled
+syncthing cli config options relays-enabled set false
+
+# Setting up folder defaults
+# Creating base folder used for synchronization
+mkdir -p "$HOME/Documents/Syncthing"
+syncthing cli config defaults folder path set "$HOME/Documents/Syncthing"
+# Set default versioning type to simple
+# https://docs.syncthing.net/users/versioning#simple-file-versioning
+syncthing cli config defaults folder versioning type set simple
