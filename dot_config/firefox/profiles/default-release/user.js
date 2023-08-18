425405a9ccffa1969c0f5287c3cb9b8dc7e44e47
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2023-08-18 20:30:32 +0200

    Add minimal firefox config

---
+++ dot_config/firefox/profiles/default-release/user.js
@@ -0,0 +1,7 @@
+// Hide warning when entering "about:config"
+user_pref("browser.aboutConfig.showWarning", false);
+
+
+// PREF: Never check updates for search engines
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_auto-update-checking
+user_pref("browser.search.update",				false);
