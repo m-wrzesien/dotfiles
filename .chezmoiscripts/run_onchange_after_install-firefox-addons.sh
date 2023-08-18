c5a3954393ac0c28c3ae7e225dc026dbb0e1f98d
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2023-08-18 20:29:01 +0200

    Add addons instalation for firefox

---
+++ .chezmoiscripts/run_onchange_after_install-firefox-addons.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -A ADDONS
+
+
+# Key is addons slug and value is GUID
+# https://addons-server.readthedocs.io/en/latest/topics/api/addons.html#detail
+ADDONS=(
+    ["addy_io"]="browser-extension@anonaddy"
+    ["canvasblocker"]="CanvasBlocker@kkapsner.de"
+    ["cookie-autodelete"]="CookieAutoDelete@kennydo.com"
+    ["cookie-editor"]="c3c10168-4186-445c-9c5b-63f12b8e2c87"
+    ["decentraleyes"]="jid1-BoFifL9Vbdl2zQ@jetpack"
+    ["modify-header-value"]="jid0-oEwF5ZcskGhjFv4Kk4lYc@jetpack"
+    ["ublock-origin"]="uBlock0@raymondhill.net"
+)
+
+EXTENSIONS_DIR=".config/firefox/profiles/default-release/extensions"
+
+
+checkInstalation() {
+    local notInstalled=()
+    for name in "${!ADDONS[@]}"; do
+        if ! [ -f "$EXTENSIONS_DIR/${ADDONS[$name]}.xpi" ] > /dev/null; then
+            notInstalled+=("$name")
+        fi
+    done
+    echo "${notInstalled[@]}"
+}
+
+install() {
+    if [ "$#" -eq 0 ]; then
+        echo "No addons to install"
+        return 0
+    fi
+    echo "Following addons will be installed: $*"
+    for name in "$@"; do
+        fileURL=$(curl -s "https://addons.mozilla.org/api/v5/addons/addon/$name/" | tee ~/log.log | jq -r .current_version.file.url)
+        curl -s -o "$EXTENSIONS_DIR/${ADDONS[$name]}.xpi" "$fileURL"
+        echo "Addon \"$name\" installed"
+    done
+}
+
+mkdir -p "$EXTENSIONS_DIR"
+
+# Chech what addons needs to be installed and put them in to array
+IFS=" " read -r -a toInstall <<< "$(checkInstalation)"
+
+install "${toInstall[@]}"
