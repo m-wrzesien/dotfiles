b122f7f45d8e7e97481d73632913df5a59a91363
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2024-10-26 20:12:06 +0200

    feat(syncthing): Use internal global discovery server

--- .chezmoiscripts/run_onchange_after_configure-syncthing.sh
+++ .chezmoiscripts/run_onchange_after_configure-syncthing.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-# Disable global discovery
-# https://docs.syncthing.net/users/config#config-option-options.globalannounceenabled
-syncthing cli config options global-ann-enabled set false
+TYPE="$(chezmoi data | jq .type -r)"
+
 # Disable NAT traversal
 # https://docs.syncthing.net/users/config#config-option-options.natenabled
 syncthing cli config options natenabled set false
@@ -19,3 +18,19 @@ syncthing cli config defaults folder path set "$HOME/Documents/Syncthing"
 # Set default versioning type to simple
 # https://docs.syncthing.net/users/versioning#simple-file-versioning
 syncthing cli config defaults folder versioning type set simple
+
+if [ "$TYPE" = "private" ]; then
+    url="$(chezmoi data | jq .syncthing_url -r)"
+    # Use internal discovery server
+    sed -i "s|<globalAnnounceServer>.*</globalAnnounceServer>|<globalAnnounceServer>$url</globalAnnounceServer>|" "$XDG_CONFIG_HOME/syncthing/config.xml"
+
+    # Enable global discovery
+    # https://docs.syncthing.net/users/config#config-option-options.globalannounceenabled
+    syncthing cli config options global-ann-enabled set true
+else
+    # Disable global discovery for work syncthing
+    # https://docs.syncthing.net/users/config#config-option-options.globalannounceenabled
+    syncthing cli config options global-ann-enabled set false
+fi
+
+systemctl restart --user syncthing.service
