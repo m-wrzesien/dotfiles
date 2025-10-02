eeec728381625cf5a1b3711cdc6fe6be45d4d07a
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2025-10-02 15:26:20 +0200

    feat(helix): highlight comments in jsonnet

---
+++ dot_config/helix/runtime/queries/jsonnet/injections.scm
@@ -0,0 +1,3 @@
+; TODO: Remove this after merge and relase of version containing https://github.com/helix-editor/helix/pull/14527
+((comment) @injection.content
+ (#set! injection.language "comment"))
