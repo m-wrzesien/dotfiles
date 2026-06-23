7cdc449998079d32e7590274369b34f870aa4a55
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2026-06-23 14:12:39 +0200

    feat(macos): move more defaults to repo
    Also :
    * removed one tap from brew, as the package now is available in the default one
    * add kubernetes related and other brew packages
    * add additional binaries that weren't available in brew

--- .chezmoiscripts/macos/run_onchange_after_install-binaries.sh
+++ .chezmoiscripts/macos/run_onchange_after_install-binaries.sh
@@ -9,18 +9,18 @@ set -x
 
 echo "$PATH"
 
-JSONNET_LANGUAGE_SERVER_CHECKSUM="8b6e642374d3004c3ac35254874ed1d14203a85c979f6684fc6218f58a8072d4"
-JSONNET_LANGUAGE_SERVER_VERSION="0.16.0"
-JSONNET_LANGUAGE_SERVER_BINARY="jsonnet-language-server"
+KUBECTL_ARGO_ROLLOUT_CHECKSUM="e822127ff7a783739c23b0f9e3005910f4cb7cf5b52a281cbf41c409fa3c9e80"
+KUBECTL_ARGO_ROLLOUT_VERSION="1.9.0"
+KUBECTL_ARGO_ROLLOUT_BINARY="kubectl-argo-rollouts"
 
 MPLS_CHECKSUM="bbad22f50544576d8b255d3f3161f8868509fd50377dc5d18a03a72cfd221b16"
 MPLS_VERSION="0.17.0"
 MPLS_BINARY="mpls"
 MPLS_ARCHIVE_PATH="/tmp/mpls.tar.gz"
 
-curl -L "https://github.com/grafana/jsonnet-language-server/releases/download/v${JSONNET_LANGUAGE_SERVER_VERSION}/jsonnet-language-server_${JSONNET_LANGUAGE_SERVER_VERSION}_darwin_arm64" --output "${HOME}/.local/bin/${JSONNET_LANGUAGE_SERVER_BINARY}"
-echo "${JSONNET_LANGUAGE_SERVER_CHECKSUM} ${HOME}/.local/bin/${JSONNET_LANGUAGE_SERVER_BINARY}" | sha256sum --check --status
-chmod +x "${HOME}/.local/bin/${JSONNET_LANGUAGE_SERVER_BINARY}"
+curl -L "https://github.com/argoproj/argo-rollouts/releases/download/v${KUBECTL_ARGO_ROLLOUT_VERSION}/kubectl-argo-rollouts-darwin-arm64" --output "${HOME}/.local/bin/${KUBECTL_ARGO_ROLLOUT_BINARY}"
+echo "${KUBECTL_ARGO_ROLLOUT_CHECKSUM} ${HOME}/.local/bin/${KUBECTL_ARGO_ROLLOUT_BINARY}" | sha256sum --check --status
+chmod +x "${HOME}/.local/bin/${KUBECTL_ARGO_ROLLOUT_BINARY}"
 
 curl -L "https://github.com/mhersson/mpls/releases/download/v${MPLS_VERSION}/mpls_${MPLS_VERSION}_darwin_arm64.tar.gz" --output "${MPLS_ARCHIVE_PATH}"
 echo "${MPLS_CHECKSUM} ${MPLS_ARCHIVE_PATH}" | sha256sum --check --status
